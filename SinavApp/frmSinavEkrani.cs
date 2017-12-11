0e1fd9c29dd45d9a64afbff6f06e29f11e3f2b24
Delil Noyan <noyandelil1903@hotmail.com>
2017-12-11 13:37:24 +0300

    GroupBox Boyutlari Ayarlandi

--- SinavApp/frmSinavEkrani.cs
+++ SinavApp/frmSinavEkrani.cs
@@ -55,8 +55,9 @@ namespace SinavApp
                 SinavSuresiUyariYuzdesi = SinavSuresi * 0.1;
 
                 int gbX = 0;
-                int gbY = -210;
+                int gbY = 0;
                 SoruSayisi = 0;
+                int enBuyukSatirYuksekligi = 0;
 
                 string gelenSatir = "";
                 while (!string.IsNullOrWhiteSpace((gelenSatir = st.ReadLine())))
@@ -65,7 +66,7 @@ namespace SinavApp
 
                     if (SoruSayisi % 2 == 1)
                     {
-                        gbY += 210;
+                        gbY += enBuyukSatirYuksekligi;
                         gbX = 0;
                     }
                     else
@@ -74,16 +75,17 @@ namespace SinavApp
                     }
 
                     GroupBox gb = new GroupBox();
+                    gb.AutoSize = true;
                     gb.Width = 330;
-                    gb.Height = 200;
+                    gb.MaximumSize = new Size(330, 0);
                     gb.Location = new Point(gbX, gbY);
                     pnlSorular.Controls.Add(gb);
 
                     string[] items = gelenSatir.Split('|');
 
                     Label lbl = new Label();
-                    lbl.Width = 330;
                     lbl.Location = new Point(10, 10);
+                    lbl.MaximumSize = new Size(330, 0);
                     lbl.AutoSize = true;
                     lbl.Text = SoruSayisi + ".  " + items[0];
 
@@ -97,8 +99,21 @@ namespace SinavApp
                         rb.Location = new Point(10, rbY);
                         rbY += 20;
                         rb.Enabled = false;
+                        rb.AutoSize = true;
                         rb.CheckedChanged += Rb_CheckedChanged;
                         gb.Controls.Add(rb);
+                    }
+
+                    if (SoruSayisi % 2 ==1)
+                    {
+                        enBuyukSatirYuksekligi = gb.PreferredSize.Height;
+                    }
+                    else
+                    {
+                        if (gb.PreferredSize.Height > enBuyukSatirYuksekligi)
+                        {
+                            enBuyukSatirYuksekligi = gb.PreferredSize.Height;
+                        }
                     }
                 }
             }
@@ -129,6 +144,8 @@ namespace SinavApp
 
         private void btnBaslat_Click(object sender, EventArgs e)
         {
+            btnBaslat.Enabled = false;
+
             t = new Timer();
             t.Tick += T_Tick;
             t.Interval = 1000;
